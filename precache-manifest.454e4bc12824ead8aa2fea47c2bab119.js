d9472b8556e9fda82b535a76c69529f2f9e71ddc
RTP 2016 <richard.thomaspryce@gmail.com>
2019-12-03 21:22:19 +0000

    Updates

---
+++ precache-manifest.454e4bc12824ead8aa2fea47c2bab119.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e6d4565d5ba71853827e4bc438b5eb0",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "32525bbd5e18c9309b76",
+    "url": "/robofriends/static/css/main.2e9c0f4e.chunk.css"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/robofriends/static/js/2.814c5b91.chunk.js"
+  },
+  {
+    "revision": "32525bbd5e18c9309b76",
+    "url": "/robofriends/static/js/main.99ff80df.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
